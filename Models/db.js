3a3d088775945fe282ef7f0cde07ee3398df304e
Praveen8161 <rbalu8161@gmail.com>
2024-01-03 16:58:36 +0530

    db changes

--- Models/db.js
+++ Models/db.js
@@ -7,9 +7,9 @@ dotenv.config();
 const localURL = process.env.LOCAL_URL;
 const mongoURL = process.env.MONGO_URL;
 
-export function dbConnect() {
+export async function dbConnect() {
   try {
-    mongoose.connect(mongoURL);
+    await mongoose.connect(mongoURL);
     console.log("DataBase Connected");
   } catch (err) {
     console.log(`DataBase Not Connected: ${err}`);
